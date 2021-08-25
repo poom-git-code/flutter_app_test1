54d77ed9fd656172c4953483aaf688e651aada4d
poom-git-code <s6152410008@sau.ac.th>
2021-08-25 16:18:14 +0700

    Code V.1.....

---
+++ lib/views/frist_ui.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class FristUI extends StatelessWidget {
+  const FristUI({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'First UI',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(
+          Icons.add
+        ),
+      ),
+      
+      
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+      body: Center(
+        child: Text(
+          'Hello Everyone',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+      ),
+
+    );
+  }
+}
